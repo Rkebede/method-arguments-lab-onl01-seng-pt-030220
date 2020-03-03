920ca2667800339fed5f2a5b754fe9a1a0ff04ca
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-03 05:13:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
-def introduction(name)(code)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction(name)
+  puts "Hi, my name is #{name} and I am learning to program."
 end
